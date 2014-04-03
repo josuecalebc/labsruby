f05d722e139ba13920ec0bda0e41f794321c0865
josuecalebc <josuecaleb@gmail.com>
2014-04-02 22:28:19 -0600

    lab2

---
+++ lab2sp1/config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Lab2sp1::Application.initialize!
---
+++ lab2sp1/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lab2sp1::Application.config.session_store :cookie_store, key: '_lab2sp1_session'
