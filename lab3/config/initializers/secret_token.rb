e9304de78ee80789fe52d1caac2689558526641f
josuecalebc <josuecaleb@gmail.com>
2014-04-10 23:34:44 -0600

    lab3

---
+++ lab3/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lab3::Application.config.secret_key_base = 'f3a752cc06c436e3a313669265b4015ddf77f8297b4c8bbec0f528488c53f4e4de15805e0560af8895819725661234eda8d4ea4088fa0d2d4c3a668437bead75'
